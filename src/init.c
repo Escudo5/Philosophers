2609e137426b101700317041db0546d650c779ed
Escudo5 <sergiomargar03@gmail.com>
2025-03-13 16:04:06 +0100

    creo funcion para asignar indice creciente a cada filo

--- src/init.c
+++ src/init.c
@@ -6,9 +6,24 @@
 /*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 16:34:39 by smarquez          #+#    #+#             */
-/*   Updated: 2025/03/12 16:34:40 by smarquez         ###   ########.fr       */
+/*   Updated: 2025/03/13 16:01:50 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include <philosophers.h>
 
+void philo_init(t_philo *philo, int id)
+{    
+    philo->id = id;    
+}
 
+void init_all_philos(t_table *table)
+{
+    int i;
+    i = 0;
+    while (i < table->total_philo)
+    {
+        philo_init(&table->philos[i], i + 1);
+        i++;
+    }
+}
