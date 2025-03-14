8b0be249f76a7dbde5c410e6d0ba68f42a72948d
Escudo5 <sergiomargar03@gmail.com>
2025-03-14 16:47:15 +0100

    rutina comida de filo

--- include/philosophers.h
+++ include/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 16:25:29 by smarquez          #+#    #+#             */
-/*   Updated: 2025/03/14 16:17:38 by smarquez         ###   ########.fr       */
+/*   Updated: 2025/03/14 16:43:56 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ typedef struct s_philo
 {
 
     int id;  //id del filosofo
-    int status; // estado, comiendo, pensando, durmiendo
+    int status; // estado, comiendo, pensando, durmiendo  1, 2, 0
     int left_fork;
     int right_fork;
     long long last_meal; // ultima vez que comio en ms
--- src/life.c
+++ src/life.c
@@ -6,7 +6,7 @@
 /*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 15:56:17 by smarquez          #+#    #+#             */
-/*   Updated: 2025/03/14 16:11:32 by smarquez         ###   ########.fr       */
+/*   Updated: 2025/03/14 16:46:16 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,12 @@ void philo_routine(void *philo)
    t_philo *ph = (t_philo *)philo;
    while (get_time() - ph->last_meal < ph->table->time_to_die && ph->table->sim_running)
    {
-    
+        pthread_mutex_lock(&ph->table->forks[ph->left_fork]);
+        pthread_mutex_lock(&ph->table->forks[ph->right_fork]);
+        ph->status = 1;
+        ph->last_meal = get_time();
+        usleep(ph->table->time_to_eat * 1000);
+        pthread_mutex_unlock(&ph->table->forks[ph->left_fork]);
+        pthread_mutex_unlock(&ph->table->forks[ph->right_fork]);
    }
 }
