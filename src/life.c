3b39656c051a66c08dab813699e61cb4a41d3a2d
Escudo5 <sergiomargar03@gmail.com>
2025-03-14 17:15:29 +0100

    rutina de filos acabada ?

--- src/life.c
+++ src/life.c
@@ -6,7 +6,7 @@
 /*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 15:56:17 by smarquez          #+#    #+#             */
-/*   Updated: 2025/03/14 16:46:16 by smarquez         ###   ########.fr       */
+/*   Updated: 2025/03/14 17:14:40 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,26 @@ void philo_routine(void *philo)
    {
         pthread_mutex_lock(&ph->table->forks[ph->left_fork]);
         pthread_mutex_lock(&ph->table->forks[ph->right_fork]);
+        pthread_mutex_lock(&ph->table->print_lock);
+        printf("Ha cogido 2 tenedores \n");
+        pthread_mutex_unlock(&ph->table->print_lock);
         ph->status = 1;
         ph->last_meal = get_time();
+        pthread_mutex_lock(&ph->table->print_lock);
+        printf("comiendo");
+        pthread_mutex_unlock(&ph->table->print_lock);
         usleep(ph->table->time_to_eat * 1000);
         pthread_mutex_unlock(&ph->table->forks[ph->left_fork]);
         pthread_mutex_unlock(&ph->table->forks[ph->right_fork]);
+        ph->status = 0;
+        pthread_mutex_lock(&ph->table->print_lock);
+        printf("durmiendo");
+        pthread_mutex_unlock(&ph->table->print_lock);
+        usleep(ph->table->time_to_sleep * 1000);
+        ph->status = 2;
+        pthread_mutex_lock(&ph->table->print_lock);
+        printf("pensando");
+        pthread_mutex_unlock(&ph->table->print_lock);
+        usleep(1000);
    }
 }
